5639617054b1c8642af426e939d697b702171f2d
Alioucbd <40865590+Alioucbd@users.noreply.github.com>
2019-01-17 09:13:45 +0000

    Update XJ_Convertor.py

--- XJ_Convertor.py
+++ XJ_Convertor.py
@@ -1,59 +1,123 @@
-import sys
-import xml.etree.ElementTree as ET
-import requests
-from extractXML import extractXML
-from validationCMD import validationCMD
-from validerJson import validerJson
-#
-fichierJson="js.json"
-argv2=True
-argv3=True
-argv4=True
-argv5=True
-argv6=True
-argv7=True
-#fonction qui permet de valider la commande saisie
-validationCMD(argv2,argv3,argv4,argv5,argv6,argv7)
+import svgwrite
+def genererSVG(nomEntite,nom,prenom,card1,nomEntite2,intitule,card2,nomAssos,date,formt):
+	if formt=="JSON":
+		nom=""
+		prenom=""
+		card2=""
+		card1=""
+		intitule=""
+		date=""
+	dwg = svgwrite.Drawing('projet.svg')
+		
+		# CREATION D'UNE ENTITE
+	dwg.add(dwg.rect((10, 10), (200, 100),
+		stroke=svgwrite.rgb(0,0, 0, '%'),
+		fill='white')
+	)
+		
+		# ASSOCIER LA CARDINALITE A L'ENTITE
+	dwg.add(dwg.text(card1,
+	    insert=(65,125),
+	    stroke='none',
+	    fill=svgwrite.rgb(15, 15, 15, '%'),
+	    font_size='15px',
+	    font_weight="bold")
+	)
+		# LIGNE RELIANT DEUX CLASSE
+	dwg.add(dwg.line((210, 40), (8, 40), 
+		stroke=svgwrite.rgb(0,0,0, '%'))
+	)
 
-#fonction qu permet de faire l'extraction au niveau du fichier XML
-def validerXml():
-	if sys.argv[4]=="-h" or sys.argv[3]=="-h":
-		r = requests.get("http://sgbdproject.000webhostapp.com/files/qtDTO.html")
-		#creation du fichier fluxXml.xml
-		fichier = open("fluxXml.xml","w")
-		fichier.write(r.text)
-		fichier.close()
-		tree = ET.parse("fluxXml.xml")
-		root = tree.getroot()
-		extractXML(root,arg3)
-	else:
-		if sys.argv[5]=="SGBD.xml":
-			tree = ET.parse(sys.argv[5])
-			root = tree.getroot()
-			extractXML(root,arg3)
-		if sys.argv[4]=="SGBD.xml":
-			tree = ET.parse(sys.argv[4])
-			root = tree.getroot()
-			extractXML(root,arg3)
+		# NOM DE L'ENTITE
+	dwg.add(dwg.text(nomEntite,
+	    insert=(65,25),
+	    stroke='none',
+	    fill='black',
+	    font_size='15px',
+	    font_weight="bold",
+	    font_family="Arial")
+	)
+	dwg.add(dwg.text(nom,
+	    insert=(65,67),
+	    stroke='none',
+	    fill='black',
+	    font_size='12px',
+	    font_weight="bold",
+	    font_family="Arial")
+	)
+	dwg.add(dwg.text(prenom,
+	    insert=(65,55),
+	    stroke='none',
+	    fill='black',
+	    font_size='12px',
+	    font_weight="bold",
+	    font_family="Arial")
+	)
+		
+		# 
+	dwg.add(dwg.rect((10, 400), (200, 100),
+	    stroke=svgwrite.rgb(0, 0, 0, '%'),
+	    fill='white')
+	)
 	
-#controle sur les arguments d'entree
-if (sys.argv[0]=="XJ_Convertor.py" and argv2 and argv3 and argv4 and argv5 and argv6):
-	if sys.argv[2]=="xml":
-		arg3=sys.argv[3]
-		validerXml()
-	else:
+	# 
+	dwg.add(dwg.text(nomEntite2,
+	    insert=(70, 420),
+	    fill='black',
+	    font_size='15px',
+	    font_weight="bold",
+	    font_family="Arial")
+	)
+	dwg.add(dwg.text(intitule,
+	    insert=(70, 450),
+	    fill='black',
+	    font_size='15px',
+	    font_weight="bold",
+	    font_family="Arial")
+	)
+		
+	
+	# 
+	dwg.add(dwg.text(card2,
+	    insert=(65,395),
+	    stroke='none',
+	    fill=svgwrite.rgb(15, 15, 15, '%'),
+	    font_size='15px',
+	    font_weight="bold")
+		)
+	
+	dwg.add(dwg.line((100, 110), (100, 400), 
+		stroke=svgwrite.rgb(0,0,0, '%'))
+	)
+	
+	
+		#l'association
+	dwg.add(dwg.line((210, 430), (8, 430), 
+		stroke=svgwrite.rgb(0,0,0, '%'))
+	)
+	dwg.add(dwg.circle(center=(100,250),
+	    r=60, 
+	    stroke=svgwrite.rgb(15, 15, 15, '%'),
+		    fill='white')
+	)
 
-		if sys.argv[4]=="-h" or sys.argv[3]=="-h":
-			r = requests.get("http://sgbdproject.000webhostapp.com/files/cnpbw.html")
-			#creation du fichier fluxXml.xml
-			fichier = open("fluxhttp.json","w")
-			fichier.write(r.text)
-			fichier.close()
-			validerJson("fluxhttp.json")
-		else :
-			validerJson(fichierJson)
-
-else :
-	print (" Commande incorrecte!")
-	print (" Elle est sous la forme : XJ_Convertor -i xml/json [-t][-h Url_FluxHTTP] -f monfichier.xml -o nomFichier.svg")
-# SAUVEGARDER LE FICHIER SVG
+	dwg.add(dwg.text(nomAssos,
+	    insert=(70, 250),
+	    fill='black',
+	    font_size='15px',
+	    font_weight="bold",
+	    font_family="Arial")
+	)
+	dwg.add(dwg.text(date,
+	    insert=(70, 270),
+	    fill='black',
+	    font_size='15px',
+	    font_weight="bold",
+	    font_family="Arial")
+	)
+	dwg.add(dwg.line((40, 255), (160, 255), 
+		stroke=svgwrite.rgb(0,0,0, '%'))
+	)
+	# output our svg image as raw xml
+	print(dwg.tostring())
+	dwg.save()	
