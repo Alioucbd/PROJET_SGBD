54f742bb2497eb8f623741b07a3fa1d9b789ab38
Alioucbd <40865590+Alioucbd@users.noreply.github.com>
2019-01-17 00:05:25 +0000

    Update XJ_Convertor.py
    fichier principal

--- XJ_Convertor.py
+++ XJ_Convertor.py
@@ -3,20 +3,21 @@ import xml.etree.ElementTree as ET
 import requests
 from extractXML import extractXML
 from validationCMD import validationCMD
-
+from validerJson import validerJson
 #
-
+fichierJson="js.json"
 argv2=True
 argv3=True
 argv4=True
 argv5=True
 argv6=True
 argv7=True
-
+#fonction qui permet de valider la commande saisie
 validationCMD(argv2,argv3,argv4,argv5,argv6,argv7)
+
 #fonction qu permet de faire l'extraction au niveau du fichier XML
 def validerXml():
-	if sys.argv[5]=="-h":
+	if sys.argv[4]=="-h" or sys.argv[3]=="-h":
 		r = requests.get("http://sgbdproject.000webhostapp.com/files/qtDTO.html")
 		#creation du fichier fluxXml.xml
 		fichier = open("fluxXml.xml","w")
@@ -34,33 +35,24 @@ def validerXml():
 			tree = ET.parse(sys.argv[4])
 			root = tree.getroot()
 			extractXML(root,arg3)
-#fonction qui permet de faire l'extraction au niveau du fichier JSON
-
-def validerJson():
-	r = requests.get("")
-	#creation du fichier fluxXml.xml
-	fichier = open("fluxXml.json","w")
-	fichier.write(r.text)
-	fichier.close()
-	tree = ET.parse("fluxXml.json")
-	root = tree.getroot()
-	extractJSON(root,arg3)
-	if sys.argv[5]=="SGBD.json":
-		tree = ET.parse(sys.argv[5])
-		root = tree.getroot()
-		extractJSON(root,arg3)
-	if sys.argv[4]=="SGBD.json":
-		tree = ET.parse(sys.argv[4])
-		root = tree.getroot()
-		extractJSON(root,arg3)
-
-#print sys.argv[0],argv2,argv3,argv4,argv5,argv6
+	
+#controle sur les arguments d'entree
 if (sys.argv[0]=="XJ_Convertor.py" and argv2 and argv3 and argv4 and argv5 and argv6):
 	if sys.argv[2]=="xml":
 		arg3=sys.argv[3]
 		validerXml()
 	else:
-		validerJson()
+
+		if sys.argv[4]=="-h" or sys.argv[3]=="-h":
+			r = requests.get("http://sgbdproject.000webhostapp.com/files/cnpbw.html")
+			#creation du fichier fluxXml.xml
+			fichier = open("fluxhttp.json","w")
+			fichier.write(r.text)
+			fichier.close()
+			validerJson("fluxhttp.json")
+		else :
+			validerJson(fichierJson)
+
 else :
 	print (" Commande incorrecte!")
 	print (" Elle est sous la forme : XJ_Convertor -i xml/json [-t][-h Url_FluxHTTP] -f monfichier.xml -o nomFichier.svg")
